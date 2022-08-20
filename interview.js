d914ce7a1c533b3e1bc013ea94f2d6d24785aec1
The Coders Hack <computersciencechamp@gmail.com>
2022-08-20 19:58:53 +0530

    added files

--- interview.js
+++ interview.js
@@ -2,4 +2,12 @@ for(let i=0;i<10;i++)
 console.log("value of i "+ i);
 
 
+<<<<<<< Updated upstream
 // This is my second stash 
+=======
+// Hello Stash changes works
+>>>>>>> Stashed changes
+
+
+/// This is third stash
+
